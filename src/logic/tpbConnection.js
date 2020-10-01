7407e0e5af3a36463e85973decc7a90f3e78e334
kushiji <kushiji@protonmail.com>
2020-10-01 22:17:47 +0200

    Updates

--- src/logic/tpbConnection.js
+++ src/logic/tpbConnection.js
@@ -1,14 +1,14 @@
 export function ajaxSearch(title) {
-    fetch('https://pirateproxy.surf/api?url=/q.php?q=' + title.toString(), {
-        crossDomain: true,
-        mode: 'cors',
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'}
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(function(err) { console.log('Fetch Error :-S', err) })
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "getData.php", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onreadystatechange = () => {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
+            console.log(JSON.parse(this.responseText));
+    };
+    xhttp.send("title=" + title.toString());
 };
+
 
 export function imdbSearch(title) {
 
