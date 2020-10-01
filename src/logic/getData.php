15244af06fd3c010d1105b15d9edfe973f22eb0b
kushiji <kushiji@protonmail.com>
2020-10-01 22:51:21 +0200

    Updates

--- src/logic/getData.php
+++
@@ -1,7 +0,0 @@
-<?php
- header("Access-Control-Allow-Origin: *");
-
-$data = file_get_contents('https://pirateproxy.surf/api?url=/q.php?q' . $_POST["title"]);
-echo json_encode($data);
-?>
-
--- src/logic/tpbConnection.js
+++ src/logic/tpbConnection.js
@@ -1,12 +1,8 @@
 export function ajaxSearch(title) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "getData.php", true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.onreadystatechange = () => {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
-            console.log(JSON.parse(this.responseText));
-    };
-    xhttp.send("title=" + title.toString());
+    fetch('https://pirateproxy.surf/api?url=/q.php?q=' + title.toString())
+        .then(response => response.json())
+        .then(data => console.log(data));
+
 };
 
 
